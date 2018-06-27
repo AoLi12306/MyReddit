89c9411f4012fb2f7be34d7fc5a9dd171043a046
ao li <davidli@enfec.com>
2018-06-27 02:39:25 -0700

    Revert "Updates"
    This reverts commit 47019d82725ce43c432f8f229555c4eecbbc2f21.

---
+++ webpack.config.js
@@ -0,0 +1,53 @@
+const path = require('path');
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const htmlWebpackPlugin = new HtmlWebpackPlugin({
+    template: path.join(__dirname, "src/index.html"),
+    filename: "./index.html"
+});
+const config = {
+    entry: path.join(__dirname, "src/index.js"),
+    output:{
+        path: path.join(__dirname, "dist"),
+        publicPath: '/',
+        filename:'bundle.js',
+    },
+    module:{
+      rules:[
+          {
+            test:/\.(js|jsx)$/,
+            exclude:/node_modules/,
+            use: {
+              loader: 'babel-loader',
+              options: {
+                cacheDirectory: true,
+                plugins: [
+                [
+                  'transform-runtime',
+                  {
+                    polyfill: false,
+                    regenerator: true,
+                  },
+                ],
+                ],
+              }
+            }
+          },
+          {
+            test: /\.css$/,
+            use: ["style-loader", "css-loader"]
+          },
+      ]
+    },
+    plugins: [htmlWebpackPlugin],
+    resolve: {
+        extensions: [".js", ".jsx"]
+    },
+    devServer:{
+        //inline:true,//实时刷新
+        port:7777,
+        historyApiFallback: true,
+        contentBase: './'
+    }
+}
+
+module.exports = config;
